41d781de41850c8d86abacbaf72945d0dc28a044
tariqul Islam <tariqul.islam.rony@gmail.com>
2020-02-16 22:02:56 +0900

    add the function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { withTranslation } from 'react-i18next'
+import { withTranslation, Trans } from 'react-i18next'
 
 class  App extends React.Component {
   constructor(props) {
@@ -16,18 +16,43 @@ class  App extends React.Component {
     this.props.i18n.changeLanguage(newLang)
   }
  
+  renderRadioButtons = () => {
+    return (
+      <div><input
+      checked={this.state.value === 'en'}
+      name="language" onChange={(e) => this.onLanguageHandle(e)} value="en" type="radio" />English &nbsp; 
+      <input name="language" value="jp" 
+      checked={this.state.value === 'jp'}
+      type="radio" onChange={(e) => this.onLanguageHandle(e)} />Japanese</div>
+    )
+  }
+
   render () {
     const {t} = this.props
     console.log('this is', this)
     return (
       <div className="App">
-        <div><input
-        checked={this.state.value === 'en'}
-        name="language" onChange={(e) => this.onLanguageHandle(e)} value="en" type="radio" />English &nbsp; 
-        <input name="language" value="jp" 
-        checked={this.state.value === 'jp'}
-        type="radio" onChange={(e) => this.onLanguageHandle(e)} /></div>
-        <p> {t('introduction')}</p>
+         { this.renderRadioButtons() }
+        <h1><Trans>Paragraph</Trans></h1>
+        <table>
+          <tbody>
+            <tr>
+              <td style={{width: '20%'}}>{t('author.title')}</td>
+              <td style={{width: '5%'}}>:</td>
+              <td style={{width: '75%'}}>{t('author.value')}</td>
+            </tr>
+            <tr>
+              <td style={{width: '20%'}}>{t('description.title')}</td>
+              <td style={{width: '5%'}}>:</td>
+              <td style={{width: '75%'}}>{t('description.value')}</td>
+            </tr>
+            <tr>
+              <td style={{width: '20%'}}>{t('description.title')}</td>
+              <td style={{width: '5%'}}>:</td>
+              <td style={{width: '75%'}}>{t('description.value')}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
--- src/i18n.js
+++ src/i18n.js
@@ -1,12 +1,12 @@
 import i18n from 'i18next'
 import LanguageDetector from "i18next-browser-languagedetector"
 import {initReactI18next} from 'react-i18next'
-//import XHR from 'i18next-xhr-backend'
+import XHR from 'i18next-xhr-backend'
 import languageEN from './locate/en/translate.json'
 import languageJP from './locate/jp/translate.json'
 
 i18n
-//.use(XHR)
+.use(XHR)
 .use(LanguageDetector)
 .use(initReactI18next)
 .init({
@@ -14,21 +14,24 @@ i18n
         en: languageEN,
         jp: languageJP
     },
+    /* default language when load the website in browser */
     lng: "en",
+    /* When react i18next not finding any language to as default in borwser */
     fallbackLng: "en",
+    /* debugger For Development environment */
     debug: true,
     ns: ["translations"],
     defaultNS: "translations",
-    keySeparator: false,
+    keySeparator: ".",
     interpolation: {
         escapeValue: false,
         formatSeparator: ","
     },
     react: {
         wait: true,
-        //bindI18n: 'languageChanged loaded',
-       // bindStore: 'added removed',
-        //nsMode: 'default'
+        bindI18n: 'languageChanged loaded',
+        bindStore: 'added removed',
+        nsMode: 'default'
     }
 })
 
